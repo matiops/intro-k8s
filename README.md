eeb933eb06c69af42905e7a54b459dc764af82be
MatiOps <ceballos.matias.lionel@gmail.com>
2024-01-09 21:20:34 -0300

    contador and saludo manifests working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # intro-k8s
 Repositorio con manifiestos de kubernetes para la parte practica de la introduccion conceptual a kubernetes.
+microk8s start
+microk8s config > ~/.kube/microk8s
+export KUBECONFIG=~/.kube/microk8s 
--- apps/app-contador/src/app.py
+++ apps/app-contador/src/app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-redis_client = Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_client = Redis(host='localhost', port=6379, db=0, decode_responses=True) # En docker compose reemplazar "localhost" por "redis"
 # Healthckech ---------
 health = HealthCheck()
 def redis_available():
